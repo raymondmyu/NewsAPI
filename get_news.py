4a460f93e8b1fd7967dc900313793b6cdb6db53a
Raymond Yu <raymond.m.yu@gmail.com>
2018-02-10 09:30:17 -0600

    Added ability to run where you left off into a sqlite db

--- get_news.py
+++ get_news.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys, os
+import sqlalchemy as sa
 from newsapi import NewsApiClient
 
 def get_and_count(requestcount, **kwargs):
@@ -7,19 +8,41 @@ def get_and_count(requestcount, **kwargs):
     requestcount += 1
     return everything, requestcount
 
+def get_last_article(sqlite):
+    ''' Find last article by looking at last source id and last date '''
+    print('Getting last article')
+    # Find last source
+    last_source = pd.read_sql("select max(source) source from all_articles", sqlite).iloc[0,0]
+    # Find last date
+    last_date = pd.read_sql("select min(publishedAt) publishedAt from all_articles where source=='{}'".format(last_source),
+                            sqlite, parse_dates=['publishedAt']).iloc[0,0]
+
+    return last_source, last_date
+
+
 if __name__=='__main__':
     newsapi = NewsApiClient(api_key='07be0d6ef4144691bf9075df390b2ef1')
 
     df_sources = pd.read_csv('sources.csv')
-    df_sources_en = df_sources.query("language=='en'")
+    df_sources_en = df_sources.query("language=='en'").sort_values('id').set_index('id')
 
     fromdate = sys.argv[1]
     todate = sys.argv[2]
     totalrequests = int(sys.argv[3])
     fname = sys.argv[4]
 
+    if os.path.exists(fname):
+        sqlite = sa.create_engine('sqlite:///{}'.format(fname)).connect()
+        last_source, last_date = get_last_article(sqlite)
+        todate = last_date.strftime('%Y-%m-%d %H:%M:%S')
+        df_sources_en = df_sources_en.loc[last_source:]
+    else:
+        sqlite = sa.create_engine('sqlite:///{}'.format(fname)).connect()
+
+    ids = df_sources_en.index
+
     requestcount = 0
-    for id in df_sources_en.id:
+    for id in ids:
         if requestcount > totalrequests: break
         print('Getting current source from', id)
         everything, requestcount = get_and_count(requestcount, sources=[id], from_parameter=fromdate, to=todate,
@@ -43,10 +66,7 @@ if __name__=='__main__':
 
             if (requestcount % 10 == 0) & (requestcount > 0):
                 print('Writing to', fname)
-                if os.path.exists(fname):
-                    cur_source_articles.to_csv(fname, mode='a', header=False, index=False)
-                else:
-                    cur_source_articles.to_csv(fname, index=False)
+                cur_source_articles.to_sql('all_articles', sqlite, index=False, if_exists='append')
 
             numresults += articles_df.shape[0]
             page += 1
